25f22d7d468eb3c7a25da7609c402dd2412afebf
KimHyoungGeun <36186887+KimHyoungGeun@users.noreply.github.com>
2018-02-07 17:32:09 +0900

    20180207 수업
    FSM, Sanse 구현

---
+++ Assets/Scripts/TankAi.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class TankAi : MonoBehaviour {
+
+    private GameObject player;
+    private Animator animator;
+    private Ray ray;
+    private RaycastHit hit;
+    private float maxDistanceToCheck = 6f;
+    private float currentDistance;
+    private Vector3 checkDirection;
+
+    public Transform pointA;
+    public Transform pointB;
+    public NavMeshAgent navMeshAgent;
+
+    private int currentTarget;
+    private float distanceFromTarget;
+    private Transform[] waypoint = null;
+
+    private void Awake () {
+        player = GameObject.FindWithTag("Player");
+        animator = gameObject.GetComponent<Animator>();
+        pointA = GameObject.Find("p1").transform;
+        pointB = GameObject.Find("p2").transform;
+
+        navMeshAgent = gameObject.GetComponent<NavMeshAgent>();
+        waypoint = new Transform[2] { pointA, pointB };
+        currentTarget = 0;
+        navMeshAgent.SetDestination(waypoint[currentTarget].position);
+    }
+
+    private void FixedUpdate () {
+        currentDistance = Vector3.Distance(player.transform.position, transform.position);
+        animator.SetFloat("distanceFromPlayer", currentDistance);
+
+        checkDirection = player.transform.position - transform.position;
+        ray = new Ray(transform.position, checkDirection);
+        if (Physics.Raycast(ray, out hit, maxDistanceToCheck))
+        {
+            if (hit.collider.gameObject == player)
+                animator.SetBool("isPlayerVisible", true);
+            else
+                animator.SetBool("isPlayerVisible", false);
+
+        }
+        else
+        {
+            animator.SetBool("isPlayerVisible", false);
+        }
+
+        distanceFromTarget = Vector3.Distance(waypoint[currentTarget].position, transform.position);
+        animator.SetFloat("distanceFromWaypoint", distanceFromTarget);
+    }
+
+    public void SetNextPoint()
+    {
+        switch (currentTarget)
+        {
+            case 0:
+                currentTarget = 1;
+                break;
+            case 1:
+                currentTarget = 0;
+                break;
+        }
+        navMeshAgent.SetDestination(waypoint[currentTarget].position);
+    }
+}
---
+++ Assets/Scripts/Wander.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wander : MonoBehaviour {
+
+    private Vector3 tarPos;
+
+    private float movementSpeed = 5f;
+    private float rotSpeed = 2f;
+    private float minX, maxX, minZ, maxZ;
+
+    void Start()
+    {
+        minX = -45f;
+        maxX = 45f;
+
+        minZ = -45f;
+        maxZ = 45f;
+
+        GetNextPosition();
+    }
+
+    void Update () {
+        if (Vector3.Distance(tarPos, transform.position) <= 5f)
+            GetNextPosition();
+        Quaternion tarRot = Quaternion.LookRotation(tarPos - transform.position);
+        transform.rotation = Quaternion.Slerp(transform.rotation, tarRot, rotSpeed * Time.deltaTime);
+        transform.Translate(new Vector3(0, 0, movementSpeed * Time.deltaTime));
+	}
+
+    void GetNextPosition()
+    {
+        tarPos = new Vector3(Random.Range(minX, maxX), 0.5f, Random.Range(minZ, maxZ));
+    }
+}
